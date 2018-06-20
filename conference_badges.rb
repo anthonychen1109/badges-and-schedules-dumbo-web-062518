d0d530d2e411d08f7b9e17a289ce9b4f822d166c
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-20 02:31:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,7 @@ def batch_badge_creator(name_array)
 end
 
 def assign_rooms(speaker_list)
-  speaker_list.each_with_index do
+  speaker_list.each_with_index do |speaker, index=1|
+    "Hello, #{speaker}! You'll be assigned to room #{index}"
+  end
 end
